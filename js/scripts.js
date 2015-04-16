1dfc496d34304a015bb1c9ae8a7860e0057a04bb
Sunny Lam <sunny.lam@gmail.com>
2015-04-16 17:35:09 -0400

    Added the jQuery slider functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,21 @@ jQuery(document).ready(function($) {
 //////////////////////////////////////////////////
 // FUNCTIONS
 
-
+function heroBlockSlider041515a () {
+	// you must use the class to call it
+	$('#bxslider').bxSlider({
+		controls: true,
+		// auto: true,
+		// slideWidth: 1440,
+		autoControls: true
+	});
+}
 
 
 //////////////////////////////////////////////////
 // EXECUTION CODE
 
+heroBlockSlider041515a();
 
 //////////////////////////////////////////////////
 
